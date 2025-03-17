b9ca6d10f5d48bb89722c6ef02e6823da17998a6
abrget <ethioanimatedclips@gmail.com>
2025-03-17 16:50:16 +0300

    last

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/status', function () {
+    $title = "Status";
+    return view('status', compact('title'));
+});
+Route::get('/spot', function () {
+    $title = "Spot Balance";
+    return view('spot', compact('title'));
+});
+Route::get('/funding', function () {
+    $title  = "Funding Balance";
+    return view('funding', compact('title'));
+});
+Route::get('/withdraw', function () {
+    $title  = "Withdraw Balance";
+    return view('withdraw', compact('title'));
+});
+
+
+Route::post('status', [UserController::class, 'status'])->name('status');
+Route::post('spot', [UserController::class, 'spot'])->name('spot');
+Route::post('funding', [UserController::class, 'funding'])->name('funding');
+Route::post('withdraw', [UserController::class, 'withdraw'])->name('withdraw');
+
+
+
+Route::post('/profile', function () {
+    return view('profile'); // Loads profile.blade.php
+})->name('profile');
+
